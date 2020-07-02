9ad801bc9f2fcf1f3e352bcbd852483557af3c58
Doriel aboya <doriel494@gmail.com>
2020-07-02 10:19:16 +0300

    Updates

---
+++ precache-manifest.28c8c51f75b295e42554cdc67651ff49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0252e8781e557d4288f3f4fc386cf9f",
+    "url": "/ww-news/index.html"
+  },
+  {
+    "revision": "b90d13ba9870831f244a",
+    "url": "/ww-news/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "545a0a357d214de2cc19",
+    "url": "/ww-news/static/css/main.c5e1b24a.chunk.css"
+  },
+  {
+    "revision": "b90d13ba9870831f244a",
+    "url": "/ww-news/static/js/2.34ec95e0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ww-news/static/js/2.34ec95e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545a0a357d214de2cc19",
+    "url": "/ww-news/static/js/main.1af99fc6.chunk.js"
+  },
+  {
+    "revision": "5c803e981ccef069a00a",
+    "url": "/ww-news/static/js/runtime-main.30155d92.js"
+  }
+]);
