b12b10f2d49f03f9f8f591f462a5648e0ba4f89f
Doriel aboya <doriel494@gmail.com>
2020-07-03 11:36:12 +0300

    errors fixed

--- src/components/Story.js
+++ src/components/Story.js
@@ -13,19 +13,19 @@ export default class Story extends Component {
         if (this.props.size==="small") {
         return (
                 <div id="allStory"  >
-                    <div class="container-fluid">
-        <div class="col-12 mt-3" style={{marginTop:0}}>
-            <div class="card">
-                <div class="card-horizontal">
-                    <div class="img-square-wrapper">
-                        <img class="storyImg" src={this.props.story.urlToImage} alt="Card image cap"/>
+                    <div className="container-fluid">
+        <div className="col-12 mt-3" style={{marginTop:0}}>
+            <div className="card">
+                <div className="card-horizontal">
+                    <div className="img-square-wrapper">
+                        <img className="storyImg" src={this.props.story.urlToImage} alt="Card  cap"/>
                     </div>
-                    <div class="card-body">
+                    <div className="card-body">
                     <a href={this.props.story.url} className="card-title title" style={{ fontSize: 15}}><h4>{this.props.story.title}</h4></a>
-                        <p class="card-text">{this.props.story.content}</p>
+                        <p className="card-text">{this.props.story.content}</p>
                     </div>
                 </div>
-                <div class="card-footer">
+                <div className="card-footer">
                 <p className="card-text" id="StoryAbout">
                         <small className="text-muted"><span>{this.props.story.source.name}</span> | <span>{this.props.story.publishedAt}</span></small>
                         </p>
@@ -41,7 +41,7 @@ export default class Story extends Component {
             <div id="allStory"  >
                 
                         <div className="card mb-3" style={{marginTop:15}}>
-                        <img className="card-img-top" id="img" style={{height: 150}}  src ={this.props.story.urlToImage} />
+                        <img className="card-img-top" id="img" style={{height: 150}}  src ={this.props.story.urlToImage} alt ="theme" />
                     </div>
                     <div style={{paddingTop:0 , paddingBottom:0}} className="card-body">
                     <div style={{textAlign:"left", fontSize: 13 , fontWeight: "bolder"}}><a href={this.props.story.url} className="card-title title" style={{ fontSize: 18 , textAlign:"left"}}>{this.props.story.title}</a>
